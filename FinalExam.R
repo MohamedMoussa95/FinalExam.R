496dda2c70cc291bd04e10dc3096c41099a0a027
MohamedMoussa95 <57390286+MohamedMoussa95@users.noreply.github.com>
2020-02-07 18:27:13 +0100

    FinalExam.R
    Final version

---
+++ FinalExam.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+#Installed Tidyverse
+
+#Import dataset from readr
+("~/Desktop/Violence & death/rate-of-violent-deaths-state-societies.csv")
+#Copied the code into the script
+
+
+#copied code into the script
+
+
+#File appears in Environment -> Global Environment
+
+
+#Copied code from Hax.odsbjerg.dk
+rate_of_death %>%
+ggplot(aes(x = Year, y = `Rate of violent deaths (state societies);`)) +
+  geom_col() +
+  xlab(NULL) +
+  labs(x = "Year",
+       y = "Rate of violent deaths (per 100.000)",
+       title = "Rate of violent deaths (state societies)")
